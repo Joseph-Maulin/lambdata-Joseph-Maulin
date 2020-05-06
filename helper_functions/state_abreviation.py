c0732db9e4782d0587228c6819452afa756eef4a
Joe Maulin <Joe.Maulin@rfiusa.com>
2020-05-06 18:35:32 -0400

    added state_abreviation.py

---
+++ helper_functions/state_abreviation.py
@@ -0,0 +1,84 @@
+def state_abreviation(state):
+    """
+        returns state name if abreviation
+        and state abreviation if name
+    """
+
+    states_dict = {"Alabama": "AL",
+              "Alaska":	"AK",
+              "Arizona": "AZ",
+              "Arkansas": "AR",
+              "California":	"CA",
+              "Colorado": "CO",
+              "Connecticut": "CT",
+              "Delaware": "DE",
+              "District of Columbia": "DC",
+              "Florida":"FL",
+              "Georgia": "GA",
+              "Hawaii": "HI",
+              "Idaho": "ID",
+              "Illinois": "IL",
+              "Indiana": "IN",
+              "Iowa": "IA",
+              "Kansas":	"KS",
+              "Kentucky": "KY",
+              "Louisiana":	"LA",
+              "Maine":	"ME",
+              "Maryland":	"MD",
+              "Massachusetts":	"MA",
+              "Michigan":	"MI",
+              "Minnesota":	"MN",
+              "Mississippi":	"MS",
+              "Missouri":	"MO",
+              "Montana":	"MT",
+              "Nebraska":	"NE",
+              "Nevada":	"NV",
+              "New Hampshire":	"NH",
+              "New Jersey":	"NJ",
+              "New Mexico":	"NM",
+              "New York":	"NY",
+              "North Carolina":	"NC",
+              "North Dakota":	"ND",
+              "Ohio":	"OH",
+              "Oklahoma":	"OK",
+              "Oregon":	"OR",
+              "Pennsylvania":	"PA",
+              "Rhode Island":	"RI",
+              "South Carolina":	"SC",
+              "South Dakota":	"SD",
+              "Tennessee":	"TN",
+              "Texas":	"TX",
+              "Utah":	"UT",
+              "Vermont":	"VT",
+              "Virginia":	"VA",
+              "Washington":	"WA",
+              "West Virginia":	"WV",
+              "Wisconsin":	"WI",
+              "Wyoming":	"WY",
+              "Puerto Rico":	"PR"
+              }
+
+    try:
+        # is abreviation
+        if len(state)==2:
+            try:
+                states_dict =  {v: k for k, v in states_dict.items()}
+
+                return states_dict[state.upper()]
+
+            except:
+                return "Not a valid abbreviation"
+
+        else:
+            try:
+                return states_dict[state.title()]
+
+            except:
+                return "Not a valid state"
+
+    except:
+        return "Wrong state format"
+
+
+
+print(state_abreviation("in"))
