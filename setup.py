ca74b6a9db29a584f9f8b37932b68d495c7505e8
Joe Maulin <Joe.Maulin@rfiusa.com>
2020-05-06 19:51:47 -0400

    fixed __init__

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata-Joseph-Maulin", # the name that you will install via pip
-    version="1.0",
+    version="1.1",
     author="Joseph Maulin",
     author_email="joseph_l_maulin@gmail.com",
     description="Helper functions for data science",
-    long_description=None,
+    long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     #license="MIT",
     url="https://github.com/Joseph-Maulin/lambdata-Joseph-Maulin",
     #keywords="",
-    packages=find_packages() # ["my_lambdata"]
+    packages=find_packages() # ["helper_functions"]
 )
