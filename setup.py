2ca9a17f9230b66d04a26d36e3a707bb9f5b716d
Joe Maulin <Joe.Maulin@rfiusa.com>
2020-05-06 19:11:04 -0400

    preparing too release

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+# setup.py file
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lambdata-Joseph-Maulin", # the name that you will install via pip
+    version="1.0",
+    author="Joseph Maulin",
+    author_email="joseph_l_maulin@gmail.com",
+    description="Helper functions for data science",
+    long_description=None,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/Joseph-Maulin/lambdata-Joseph-Maulin",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
